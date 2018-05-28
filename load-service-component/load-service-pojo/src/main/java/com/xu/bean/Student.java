09a00ec5d505dcdb851ddb6f854182a689b2d1e0
xuhongda <xuhongda7@outlook.com>
2018-05-28 13:53:49 +0800

    添加bean

---
+++ load-service-component/load-service-pojo/src/main/java/com/xu/bean/Student.java
@@ -0,0 +1,22 @@
+package com.xu.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author xuhongda on 2018/5/24
+ * bean
+ * load-service-parent
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Student {
+    private String name;
+    private int age;
+
+
+}
